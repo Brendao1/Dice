adfb7c228e9b1a2ed70b748f268fb78c65f24c02
“Brenda” <odonnell.brenda@gmail.com>
2019-02-08 13:41:58 +0000

    Re-created unit test for initialize Dice. This time it takes a parameter, number of Dice.

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,21 +1,19 @@
 # the objects I need are: a dice, which gives a number between 1 and 6.
 
-class Game
-end
 
 
 class Dice
 
-  def initialize
-    dice = Dice
+  def initialize(number)
+    # dice = Dice.new(Integer)
   end
 
-  def roll
-    rand(1..6)
-  end
-
-  def num_dice(number)
-  end
+  # def roll
+  #   rand(1..6)
+  # end
+  #
+  # def num_dice(number)
+  # end
 
 
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -2,22 +2,25 @@ require 'dice'
 
 describe Dice do
 
-  it 'is created from the  initlize command' do
-    dice = Dice.new
+  it 'is created from the initlize command with one parameter' do
+    Dice.new(3)
   end
 
-  it 'rolls a random number between 1 and 6' do
-   dice = Dice.new
-   dice.roll
-   # expect(dice.roll).to be_an(Integer)
-   expect(dice.roll).to be_between(1, 6)
-  end
+  # it 'is created from the initialize command and takes 1 parameter' do
+  #   expect(Dice).to respond_to(:initialize).with(1).argument
+  # end
 
- it 'responds to num_dice method' do
-   p dice = Dice.new
-   p dice.num_dice(3)
-   # expect(dice.num(3)) to give me 3 dice
-
- end
+ #  it 'rolls a random number between 1 and 6' do
+ #   dice = Dice.new
+ #   dice.roll
+ #   # expect(dice.roll).to be_an(Integer)
+ #   expect(dice.roll).to be_between(1, 6)
+ #  end
+ #
+ # # it 'responds to num_dice method' do
+ # #   p dice = Dice.new
+ # #   p dice.num_dice(3)
+ # #   # expect(dice.num(3)) to give me 3 dice
+ # end
 
 end
