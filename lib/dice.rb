f055e89beaa4327e339a66b110eab0c83046357e
“Brenda” <odonnell.brenda@gmail.com>
2019-02-11 09:02:10 +0000

    Updated dice class and rspec.

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,19 +1,16 @@
 # the objects I need are: a dice, which gives a number between 1 and 6.
 
-
-
 class Dice
 
-  def initialize(number)
-    # dice = Dice.new(Integer)
+  def initialize
+    dice = Dice
   end
 
-  # def roll
-  #   rand(1..6)
-  # end
-  #
+  def roll
+    rand(1..6)
+  end
+
   # def num_dice(number)
   # end
-
 
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -3,20 +3,15 @@ require 'dice'
 describe Dice do
 
   it 'is created from the initlize command with one parameter' do
-    Dice.new(3)
+    Dice.new
   end
 
-  # it 'is created from the initialize command and takes 1 parameter' do
-  #   expect(Dice).to respond_to(:initialize).with(1).argument
-  # end
+  it 'rolls a random number between 1 and 6' do
+   dice = Dice.new
+   dice.roll
+   expect(dice.roll).to be_between(1, 6)
+  end
 
- #  it 'rolls a random number between 1 and 6' do
- #   dice = Dice.new
- #   dice.roll
- #   # expect(dice.roll).to be_an(Integer)
- #   expect(dice.roll).to be_between(1, 6)
- #  end
- #
  # # it 'responds to num_dice method' do
  # #   p dice = Dice.new
  # #   p dice.num_dice(3)
