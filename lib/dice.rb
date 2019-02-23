527721747aad45e002aae9930234236701b703a1
“Brenda” <odonnell.brenda@gmail.com>
2019-02-23 17:57:54 +0000

    Updated README.md

--- lib/dice.rb
+++ lib/dice.rb
@@ -1,19 +1,11 @@
 class Dice
 
-  # def initialize
-  #   dice = Dice
-  # end
-
-# why does this work when I comment out the initialize method?
-
   def roll(number_of_dice)
     scores = []
-    number_of_dice.times {
-    scores << (rand(1..6))
-  }
-  return scores
-end
+    number_of_dice.times { scores << (rand(1..6)) }
+    scores
+  end
 
 end
 
-# looked at code from https://github.com/RobertsK284/tdd-a-dice-class/blob/master/lib/dice.rb
+# compared with code from https://github.com/RobertsK284/tdd-a-dice-class/blob/master/lib/dice.rb
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -3,13 +3,8 @@ require 'dice'
 describe Dice do
   subject { Dice.new }
 
-  # it 'is created from the initlize command with one parameter' do
-  #   Dice.new
-  # end
-
   it 'rolls multiple dice, rolls a random number between 1 and 6' do
    subject.roll(2)
-   # expect(subject.roll).to be_between(1, 6)
    expect([1, 2, 3, 4, 5, 6]).to include subject.roll(2)[0], subject.roll(2)[1]
   end
 
